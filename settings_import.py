93fe6b931132706a3233c014d1d5de5f8ed729b7
Graham Gilbert <graham@grahamgilbert.com>
2015-06-10 17:38:56 +0100

    Initial, Probably broken commit

---
+++ settings_import.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+from os import getenv
+import locale
+
+# Read list of admins from $DOCKER_SAL_ADMINS env var
+admin_list = []
+if getenv('DOCKER_IMAGR_ADMINS'):
+    admins_var = getenv('DOCKER_IMAGR_ADMINS')
+    if ',' in admins_var and ':' in admins_var:
+        for admin in admins_var.split(':'):
+            admin_list.append(tuple(admin.split(',')))
+        ADMINS = tuple(admin_list)
+    elif ',' in admins_var:
+        admin_list.append(tuple(admins_var.split(',')))
+        ADMINS = tuple(admin_list)
+else:
+    ADMINS = (
+                ('Admin User', 'admin@test.com')
+             )
+
+# Read the preferred time zone from $DOCKER_SAL_TZ, use system locale or
+# set to 'America/New_York' if neither are set
+if getenv('DOCKER_IMAGR_TZ'):
+    if '/' in getenv('DOCKER_IMAGR_TZ'):
+        TIME_ZONE = getenv('DOCKER_IMAGR_TZ')
+    else: TIME_ZONE = 'America/New_York'
+elif getenv('TZ'):
+    TIME_ZONE = getenv('TZ')
+else:
+    TIME_ZONE = 'America/New_York'
+
+# Read the preferred language code from $DOCKER_SAL_LANG, use system locale or
+# set to 'en_US' if neither are set
+if getenv('DOCKER_IMAGR_LANG'):
+    if '_' in getenv('DOCKER_IMAGR_LANG'):
+        LANGUAGE_CODE = getenv('DOCKER_IMAGR_LANG')
+    else:
+        LANGUAGE_CODE = 'en_US'
+# elif locale.getdefaultlocale():
+#     LANGUAGE_CODE = locale.getdefaultlocale()[0]
+else:
+    LANGUAGE_CODE = 'en_US'
+
+# Read the list of allowed hosts from the $DOCKER_SAL_ALLOWED env var, or
+# allow all hosts if none was set.
+if getenv('DOCKER_IMAGR_ALLOWED'):
+    ALLOWED_HOSTS = getenv('DOCKER_IMAGR_ALLOWED').split(',')
+else:
+    ALLOWED_HOSTS = ['*']
+
+# Set the display name from the $DOCKER_SAL_DISPLAY_NAME env var, or
+# use the default
+if getenv('DOCKER_IMAGR_DISPLAY_NAME'):
+    DISPLAY_NAME = getenv('DOCKER_IMAGR_DISPLAY_NAME')
+else:
+    DISPLAY_NAME = 'Imagr Server'
---
+++ wsgi.py
@@ -0,0 +1,14 @@
+import os, sys
+import site
+
+IMAGR_ENV_DIR = '/home/docker/imagr'
+
+# Use site to load the site-packages directory of our virtualenv
+site.addsitedir(os.path.join(IMAGR_ENV_DIR, 'lib/python2.7/site-packages'))
+
+# Make sure we have the virtualenv and the Django app itself added to our path
+sys.path.append(IMAGR_ENV_DIR)
+sys.path.append(os.path.join(IMAGR_ENV_DIR, 'imagr_server'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imagr_server.settings")
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
